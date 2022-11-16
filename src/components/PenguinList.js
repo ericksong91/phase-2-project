95cb99c7811099d198ce2bf77b9fd4e1e60b7c20
Eric Song <ericksong91@gmail.com>
2022-11-16 13:19:59 -0500

    Added json server package and npm run server script

--- src/components/PenguinList.js
+++ src/components/PenguinList.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from 'react';
 import PenguinCard from './PenguinCard';
 
 function PenguinList() {
+
+    //Ability to add Penguins
+    //Add Header for different sections? Maybe a page that goes into more detail on the penguins
+    //
+
     const [penguins, setPenguins] = useState([]);
 
     useEffect(() => {
